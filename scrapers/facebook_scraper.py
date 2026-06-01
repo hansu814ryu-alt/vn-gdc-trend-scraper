8378a3a80ba89dea73ad20a5c7431a0c821c5e8f
hansu814ryu-alt <hansu814ryu@gmail.com>
2026-06-01 16:48:49 +0900

    Update facebook_scraper.py

--- scrapers/facebook_scraper.py
+++ scrapers/facebook_scraper.py
@@ -1,1 +1,15 @@
+from core.base_scraper import BaseScraper
 
+class FacebookGroupScraper(BaseScraper):
+    def __init__(self, target_group_name: str, cookie_path: str):
+        super().__init__(platform_name="Facebook", target_source=target_group_name)
+        self.cookie_path = cookie_path
+
+    def collect_raw_data(self):
+        # ----------------------------------------------------
+        # TODO: [A 개발자 담당 알맹이 영역]
+        # Playwright를 이용해 secrets/fb_cookies.json을 로드한 뒤
+        # 타겟 페이스북 그룹 스크래핑 및 self.standardize_format() 호출 후
+        # self.output_data.append() 하는 구조 기술
+        # ----------------------------------------------------
+        pass
