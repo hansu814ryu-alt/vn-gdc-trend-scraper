99a3882dc8e3abed70f73cbf7ba3031701f209ba
hansu814ryu-alt <hansu814ryu@gmail.com>
2026-06-01 16:49:14 +0900

    Update threads_scraper.py

--- scrapers/threads_scraper.py
+++ scrapers/threads_scraper.py
@@ -1,1 +1,13 @@
+from core.base_scraper import BaseScraper
 
+class ThreadsKeywordScraper(BaseScraper):
+    def __init__(self, search_keyword: str):
+        super().__init__(platform_name="Threads", target_source=search_keyword)
+
+    def collect_raw_data(self):
+        # ----------------------------------------------------
+        # TODO: [C 개발자 담당 알맹이 영역]
+        # Apify Actor API 혹은 사설 API 호스트 엔드포인트를 호출하여
+        # 키워드 검색 결과 텍스트 데이터를 받아 정형화 처리
+        # ----------------------------------------------------
+        pass
