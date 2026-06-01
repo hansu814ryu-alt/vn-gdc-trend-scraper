54720d5473e12f87b9b596e197447af30e297853
hansu814ryu-alt <hansu814ryu@gmail.com>
2026-06-01 16:49:02 +0900

    Update itviec_scraper.py

--- scrapers/itviec_scraper.py
+++ scrapers/itviec_scraper.py
@@ -1,1 +1,13 @@
+from core.base_scraper import BaseScraper
 
+class ITViecReviewScraper(BaseScraper):
+    def __init__(self, target_company_url: str):
+        super().__init__(platform_name="ITviec", target_source=target_company_url)
+
+    def collect_raw_data(self):
+        # ----------------------------------------------------
+        # TODO: [B 개발자 담당 알맹이 영역]
+        # BeautifulSoup를 사용해 해당 기업 리뷰 텍스트를 파싱하여
+        # 부모의 구조 양식에 맞추어 self.output_data에 적재
+        # ----------------------------------------------------
+        pass
